fd4b2fd5c946e83c93f9d94ad414006e00a26e07
akashgokul <akashgokul@berkeley.edu>
2020-05-22 10:57:47 -0400

    adding segmentation repo

--- scripts/python/data_loader.py
+++ scripts/python/data_loader.py
@@ -21,6 +21,7 @@ from __future__ import absolute_import
 # other imports
 import sys
 sys.path.append('/home/akash/core50/')
+print(sys.path)
 from core50_segmentation.delete_background_multimages import process_img
 
 import numpy as np
