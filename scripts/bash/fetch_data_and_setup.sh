af7c340ab328226f26865ef07b3a563b534fcbce
akashgokul <akashgokul@berkeley.edu>
2020-05-21 22:02:50 -0400

    editing data script in vscode to avoid err

--- scripts/bash/fetch_data_and_setup.sh
+++ scripts/bash/fetch_data_and_setup.sh
@@ -6,11 +6,11 @@ mkdir $DIR/../../data
 mkdir $DIR/../../data/logs
 mkdir $DIR/../../data/snapshots
 
-echo "Downloading caffenet pre-trained model..."
-wget --directory-prefix=$DIR'/../../data/' http://dl.caffe.berkeleyvision.org/bvlc_reference_caffenet.caffemodel
+# echo "Downloading caffenet pre-trained model..."
+# wget --directory-prefix=$DIR'/../../data/' http://dl.caffe.berkeleyvision.org/bvlc_reference_caffenet.caffemodel
 
-echo "Downloading VGG_CNN_M pre-trained model..."
-wget --directory-prefix=$DIR'/../../data/' http://www.robots.ox.ac.uk/%7Evgg/software/deep_eval/releases/bvlc/VGG_CNN_M.caffemodel
+# echo "Downloading VGG_CNN_M pre-trained model..."
+# wget --directory-prefix=$DIR'/../../data/' http://www.robots.ox.ac.uk/%7Evgg/software/deep_eval/releases/bvlc/VGG_CNN_M.caffemodel
 
 echo "Downloading Core50 (128x128 version)..."
 wget --directory-prefix=$DIR'/../../data/' http://bias.csr.unibo.it/maltoni/download/core50/core50_128x128.zip
